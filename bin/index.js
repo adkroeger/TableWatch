432af292a7894f65ace2f81c05b577cd685d7dd0
Adrian Kröger <a.kroeger@grimme.de>
2022-05-02 12:59:45 +0200

    feat(tableWatch): added table watch library

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
 
+const mysql = require("mysql");
+const mySqlEvents = require("@rodrigogs/mysql-events");
+
+const exec = async () => {
+   const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root'
+   });
+   
+   const instance = new mySqlEvents(connection, {
+      startAtEnd: true
+   });
+
+   await instance.start();
+
+   instance.addTrigger({
+      name: "monitoring all statements",
+      expression: "TestDb.*",
+      statement: mySqlEvents.STATEMENTS.ALL,
+      onEvent: (event) => {
+         console.log(event);
+      }
+   })
+};
+
+exec();
